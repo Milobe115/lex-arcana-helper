e062581b8a1adf9fb310855007108d1cd3931740
Milo Politan <milobe.115@gmail.com>
2021-01-16 23:23:59 +0100

    Delet test files

--- src/app/disable-arrows.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DisableArrowsDirective } from './disable-arrows.directive';
-
-describe('DisableArrowsDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DisableArrowsDirective();
-    expect(directive).toBeTruthy();
-  });
-});
